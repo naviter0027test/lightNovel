182c43abb6e03a64835939f56197aab174ce69ff
lanker <lanker@test0023.test0012.dlinkddns.com>
2016-07-04 14:37:14 +0800

    pager html and css finish, cp panel html, css and js finish

---
+++ js/postArticle.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    $(".cpPanel a").on("click", function() {
+        $(".cpPanel a").removeClass("nowChoose");
+        $(this).addClass("nowChoose");
+        $(".cpPanel div").hide();
+        $($(this).attr("href")).show();
+        return false;
+    });
+    $(".cpPanel button").on("click", function() {
+        $(".cpPanel").fadeOut();
+        return false;
+    });
+
+    var cpInput = null;
+
+    $(".cpPanel button.check").on("click", function() {
+        var cpDiv = $(".cpPanel a.nowChoose").attr("href");
+        var radioChoose = $(cpDiv).find("input:checked");
+        //console.log($(radioChoose).val());
+        $(cpInput).val($(radioChoose).val());
+        cpInput = null;
+    });
+
+    $("[name='cp1[]'],[name='cp2[]']").on("focus", function() {
+        cpInput = this;
+        $(".cpPanel").fadeIn();
+    });
+});
