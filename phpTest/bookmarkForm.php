25c03a62e56d3cf72930186802508ca4d7d656d6
lanker <lanker@test0023.test0012.dlinkddns.com>
2016-09-13 21:50:13 +0800

    bookmark form init

---
+++ phpTest/bookmarkForm.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title> book marks test </title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <form action="../instr.php" method="post">
+            <input type="hidden" name="instr" value="bookmark" />
+            <input type="text" name="bookId" />
+        </form>
+    </body>
+</html>
